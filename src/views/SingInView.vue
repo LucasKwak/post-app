ce470a4ecf5d6f2e12767bfb0844208effe117df
LucasKwak <lkwakiglesia@gmail.com>
2024-07-09 00:37:27 +0200

    [FEAT]: Add structure and styling to the sing-in view. Create the SignInForm component.

--- src/views/SingInView.vue
+++ src/views/SingInView.vue
@@ -1,11 +1,18 @@
 <template>
-    <h1>Sign In View</h1>
+    <div class="form-wrapper">        
+        <SignInForm></SignInForm>
+    </div>
 </template>
 
 <script lang="ts" setup>
-
+    import SignInForm from '@/components/SignInForm.vue'
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .form-wrapper {
+        padding-top: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 </style>

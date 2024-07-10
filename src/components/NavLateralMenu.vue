2b87d9c51a46f5e4ed83132630a1334e50dc4088
LucasKwak <lkwakiglesia@gmail.com>
2024-07-10 20:55:59 +0200

    [FEAT]: Add to HomeView the lateral menu to allow the user to see the posts and to create one.

---
+++ src/components/NavLateralMenu.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="nav-lateral-menu">
+        <router-link :to="{name: 'allPosts'}">All posts</router-link>
+        <router-link :to="{name: 'myPosts'}">My posts</router-link>
+        <router-link :to="{name: 'createPost'}">Create post</router-link>
+    </nav>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+    .nav-lateral-menu {
+        display: flex;
+        flex-direction: column;
+        border: 2px solid $secondaryColor;
+        border-radius: 15px;
+        padding: 20px;
+        gap: 25px;
+
+        a {
+            font-weight: bold;
+            color: $textColor;
+            text-decoration: none;
+            border: 2px solid $contrastColorLight;
+            border-radius: 15px;
+            padding: 8px;
+
+            &.router-link-exact-active {
+                color: $primaryColor;
+                border: 2px solid $primaryColor;
+            }
+        }
+    }
+</style>
